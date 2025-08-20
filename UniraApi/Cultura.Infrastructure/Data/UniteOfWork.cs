b48deffb07f0ae3a5b69ab84e1c188a22f6efee9
AscensionismSS <gabriel.douglas.xd@gmail.com>
2025-08-20 19:19:09 -0300

    ajuste

---
+++ UniraApi/Cultura.Infrastructure/Data/UniteOfWork.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cultura.Data;
+using Cultura.Infrastructure.Repositories.Interfaces;
+
+namespace Cultura.Infrastructure.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<int> CommitAsync()
+        {
+            return _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ UniraApi/Cultura.Infrastructure/Repositories/EnderecoRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cultura.Data;
+using Cultura.Domain.Entities;
+using Cultura.Infrastructure.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cultura.Infrastructure.Repositories
+{
+    public class EnderecoRepository : IEnderecoRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EnderecoRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Endereco> BuscarEnderecoUnicoAsync(string cep, string bairro, string rua, string numero)
+        {
+            return await _context.Enderecos
+                .FirstOrDefaultAsync(e => e.Cep == cep && e.Bairro == bairro && e.Rua == rua && e.Numero == numero);
+        }
+
+        public void CreateEndereco(Endereco endereco)
+        {
+            _context.Enderecos.Add(endereco);
+            
+        }
+    }
+}
+
+
